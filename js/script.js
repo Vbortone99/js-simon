a631617a37567190b5622ff391abacfda5a6711d
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-23 21:24:18 +0200

    1st template to implement in HTML

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Generare TEMPLATE da inserire
+/**
+ * TEMPLATE
+ <div class="col">
+     <div class="card d-flex justify-content-center align-items-center display-3">
+         <input type="text" class="form-control">
+ 
+     </div>
+ </div>
+*/
 
 "use strict"
 
